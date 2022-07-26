b782ef698cdcc4ed67d85802b5ce4208101105ef
Nikhil Gududas <nikhil001.dev@gmail.com>
2022-07-26 22:22:51 +0530

    login page created

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'package:blog_app/screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: LoginScreen(),
     );
